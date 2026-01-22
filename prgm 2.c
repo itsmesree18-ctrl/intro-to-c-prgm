d89c389fa2c58a99fe889f1a5db0630959b7d95c
SWETHA K S <itsmesree18@gmail.com>
2026-01-21 21:23:11 -0800

    Create prgm 2.c

---
+++ prgm 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("Your age is %d", age);
+    return 0;
+}
+
+//output
+Enter your age: 14
+Your age is 14
